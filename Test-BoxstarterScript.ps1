0c99e15f6b24b2769c5ba20b46dc8ca0caa14fce
Cyril Madigan <90966823+CyMad7001@users.noreply.github.com>
2023-10-19 15:18:46 +0100

    Updated test harness

--- Test-BoxstarterScript.ps1
+++ Test-BoxstarterScript.ps1
@@ -24,6 +24,6 @@ param (
     $ScriptPath='Madigan-Software/windows-dev-box-setup-scripts/dev_app_desktop.NET.ps1'
 )
 
-#Import-Module Boxstarter
-. { Invoke-Webrequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force;
+#. { Invoke-Webrequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 Install-BoxstarterPackage -PackageName $script -DisableReboots
