cb983d0f4bedffde89644db6ea6e31b969f8f372
Cyril.Madigan <90966823+CyMad7001@users.noreply.github.com>
2023-10-22 20:06:20 +0100

    Added githook skeletons

---
+++ .githooks/pre-commit.ps1
@@ -0,0 +1,3 @@
+## https://www.youtube.com/watch?v=ig5E8CcdM9g
+## https://stackoverflow.com/questions/5629261/running-powershell-scripts-as-git-hooks
+## https://git-scm.com/docs/githooks#_post_checkout
